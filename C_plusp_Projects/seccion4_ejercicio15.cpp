c3d34e78b8ce07e1aa8198b2e3ee64f6fc2d266a
DevTras-18 <sertrejo>
2022-03-06 22:58:07 -0700

    Newst excercise version

--- C_plusp_Projects/seccion4_ejercicio15.cpp
+++ C_plusp_Projects/seccion4_ejercicio15.cpp
@@ -1,12 +1,10 @@
-/*Ejercicio #15 Realice un programa que solicite al usuario que piense un numero entero entre el 1 y el 100.
+/*Ejercicio #15 Realice un programa que solicite al usuario ingresar un numero entero entre el 1 y el 100.
         El programa debe generar un numero aleatorio en ese mismo rango [1-100], e indicarle al usuario si el numero que digito es menor
-#include<iostream>
         o mayor al numero aleatorio, asi hasta que lo adivine y por ultimo mostrarle el numero de intentos que le llevo.*/
+
 #include<iostream>
-#include<stdio.h>
 #include<stdlib.h>
-#include<string>
-#include <cstddef>
+#include<time.h>
 
 using namespace std;
 //enum definicion [_Positivo = 1, _Negative = 0];
@@ -15,7 +13,7 @@ using namespace std;
 int main()
 {
 
-    int _tryouts = 0;
+    int _tryouts = 0,get_number;
     int _rand_values;
     string _response;
 
@@ -24,22 +22,32 @@ int main()
         //getch();
 
     Startagain:
-    _rand_values = 1 + rand() % (100-1);
+	srand(time(NULL)); //Generara numero aleatorio;
+	_rand_values = 1 + rand()%((100+1)-1); //Operacion Para generar rand number.
 
-        cout << "Did you get the correct number?\n\r"<< _rand_values << " \n\rPlease enter Yes [Y] or No [N]: " << endl;
-        getline(std::cin,_response);
+	do{
+
+		cout << "Ingrese un numero: "; cin >> get_number;
+
+		if(_rand_values>get_number)
+		{
+			cout << "\nDigite un numero menor\n";
+		}
+		if(_rand_values<get_number)
+		{ 
+
+			cout << "\nDigite un numero mayor\n";
+
+		}
+
+		_tryouts++;
 
 
-        if(_response.find_first_not_of("Y"))
-        {
-            _tryouts+=1;
 
-        }
-        else
-        {
-           goto Startagain;
-        }
+	}while(get_number != _rand_values);
 
+	cout << "\nFelicidades has ingresado el numero generado " << get_number << endl;
+	cout << "\nNumero de intentos hasta lograr exito: "<< _tryouts << endl;
 
 
     return 0;
