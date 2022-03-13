2821abcdd512e3b47d2c9f292317f5162ff224df
DevTras-18 <sertrejo>
2022-03-13 11:04:11 -0600

    Development of Calculator System

---
+++ Calculator.cpp
@@ -0,0 +1,59 @@
+#include<iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+
+	int _initvar1, _initvar2, _initvar3, _seleccion;
+	int _continue = 1, _final = 2;
+
+
+
+	while()
+	{
+	
+
+
+
+
+	}
+
+
+
+
+
+
+
+
+
+	return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
